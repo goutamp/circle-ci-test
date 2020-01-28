8c5d7de1b3011478f04e17b8393a7ab9ffb7191e
goutamp <pramanick.goutam@gmail.com>
2020-01-28 10:49:06 +0530

    update the module name

--- adfs-test/adfs.tf
+++ adfs-test/adfs.tf
@@ -1,5 +1,5 @@
 
-module "aws-adfs" {
+module "${var.module_name}" {
   source           = "git::github.com/goutamp/adfs-test.git//adfs-custom"
   app_name         = "${var.app_name}"
   app_parent       = "${var.app_parent}"
