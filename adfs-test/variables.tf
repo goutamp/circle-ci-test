0054a6560410f94f6939e70eab34e42a767dd500
goutamp <pramanick.goutam@gmail.com>
2020-01-28 11:42:17 +0530

    syntax change for the tf 12

--- adfs-test/variables.tf
+++ adfs-test/variables.tf
@@ -1,26 +1,26 @@
 variable "aws_account_name" {
   description = "AWS Account Name"
-  type        = "string"
+  type        =  string
 }
 
 variable "target_region" {
   description = "AWS Region to use whilst provisioning this infrastructure"
-  type        = "string"
+  type        =  string
   default     = "us-east-1"
 }
 variable "app_parent" {
   description = "Application parent/category"
-  type        = "string"
+  type        =  string
 }
 
 variable "app_name" {
   description = "Application Name"
-  type        = "string"
+  type        =  string
 }
 
 variable "provider_name" {
   description = "Provide the idp provider name"
-  type        = "string"
+  type        =  string
 }
 
 
